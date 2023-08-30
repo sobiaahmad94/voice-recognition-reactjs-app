81abe72123e341deee4994267663edcfb4c728d1
Sobia <sobiacodes@gmail.com>
2023-08-31 00:07:52 +0100

    chakra ui library installed and set up

--- public/main.js
+++ public/main.js
@@ -2,4 +2,7 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
+// styles
+// Chakra UI
+import { ChakraProvider } from "@chakra-ui/react";
+ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(ChakraProvider, { children: _jsx(App, {}) }) }));
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import TextToVoice from "./components/TextToVoice";
 import VoiceToText from "./components/VoiceToText";
 
+
+
 const App = () => {
   return (
     <>
-    <h1>Voice Recognition App</h1>
-    <TextToVoice/>
-    <VoiceToText/>
-
-
-
+        <h1>Voice Recognition App</h1>
+        <TextToVoice/>
+        <VoiceToText/>
     </>
 
-  )
-}
+  );
+};
 
 export default App;
